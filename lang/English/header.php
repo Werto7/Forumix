011f4beaba9dc2311097bd2aca541a954aa796d4
Werto7 <nemoschmidt8@gmail.com>
2025-07-20 11:42:46 +0200

    Add files via upload

--- lang/English/header.php
+++ lang/English/header.php
@@ -3,6 +3,7 @@ $lang_header = array(
 'index' => 'Index',
 'log_in' => 'Login',
 'log_out' => 'Logout',
-'register' => 'Register'
+'register' => 'Register',
+'loggedInAs' => 'Logged in as %s'
 );
 ?>
---
+++ lang/English/register.php
@@ -0,0 +1,16 @@
+<?php
+$lang_register = array(
+'step1' => 'Registration – Step 1',
+'insert_pgp' => 'Please enter your public PGP key:',
+'continue' => 'Continue',
+'step2' => 'Registration – Step 2',
+'user_name' => 'Username: ',
+'enc_msg' => 'Encrypted message (please decrypt):',
+'token' => 'Token:',
+'register' => 'Register',
+'error_noName' => 'Could not extract name from PGP key',
+'error_userExists' => 'User already exists',
+'error_emailFound' => 'Providing an email address is not secure',
+'error_decrypt' => 'The decrypted message is incorrect'
+);
+?>
