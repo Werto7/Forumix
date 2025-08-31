813e603ad3d0360fc3a34c4b639fa6d7e7ffb754
Werto7 <nemoschmidt8@gmail.com>
2025-08-31 21:48:55 +0200

    Add files via upload

--- Forumix/lang/German/header.php
+++ Forumix/lang/German/header.php
@@ -1,1 +1,9 @@
-
+<?php
+$lang_header = array(
+'index' => 'Index',
+'log_in' => 'Anmelden',
+'log_out' => 'Abmelden',
+'register' => 'Registrieren',
+'loggedInAs' => 'Angemeldet als %s'
+);
+?>
---
+++ Forumix/lang/German/install.php
@@ -0,0 +1,6 @@
+<?php
+$lang_install = array(
+'success' => 'Die Installation wurde erfolgreich abgeschlossen.',
+'index' => 'Du kannst jetzt zur <a href="../index.php">Startseite</a> gehen.'
+);
+?>
